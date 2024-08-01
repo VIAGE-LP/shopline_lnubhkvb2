b2f2dcd3e2739fb72ce7ebea43d64499ce98b124
Pickachu Chen <pickachu1006@gmail.com>
2024-08-01 11:28:41 +0800

    20240801_set timer time

--- js/countdown.js
+++ js/countdown.js
@@ -9,9 +9,8 @@ if ((now >= day_start) && (now <= end)) {
   setInterval(() => {
     now = new Date();
     const differ_end = end.getTime() - now.getTime();
-    
     //判斷刷新後的now，是否還在期間內
-    if(differ_end > 0){
+    if (differ_end > 0) {
       //判斷顯示天倒數or秒倒數
       if ((now >= day_start) && (now < sec_start)) {
         $("#cdtimer_day").show();
@@ -20,24 +19,28 @@ if ((now >= day_start) && (now <= end)) {
         $("#cdtimer_day").hide();
         $("#cdtimer").show();
       }
-
-      const ms = Math.floor(differ_end);
-      const sec = Math.floor(differ_end / 1000 % 60);
-      const min = Math.floor(differ_end / 1000 / 60 % 60);
-      const hour = Math.floor(differ_end / 1000 / 60 / 60 % 24);
-      const day = Math.floor(differ_end / 1000 / 60 / 60 / 24);
-  
-      document.querySelector('#countdown .miriseconds').textContent = String(ms).slice(-3, -1);
-      document.querySelector('#countdown .seconds').textContent = String(sec).padStart(2, '0');
-      document.querySelector('#countdown .minutes').textContent = String(min).padStart(2, '0');
-      document.querySelector('#countdown .hours').textContent = String(hour).padStart(2, '0');
-      for (i = 0; i <= document.querySelectorAll('#countdown .day').length; i++) {
-        if (document.querySelectorAll('#countdown .day')[i] != undefined)
-          document.querySelectorAll('#countdown .day')[i].textContent = String(day).padStart(2, '0');
-      }
-    }else{
+      timer(differ_end);
+    } else {
       $("#cdtimer_day").hide();
       $("#cdtimer").hide();
     }
   }, 1000);
 };
+
+
+function timer(differ_end) {
+  const ms = Math.floor(differ_end);
+  const sec = Math.floor(differ_end / 1000 % 60);
+  const min = Math.floor(differ_end / 1000 / 60 % 60);
+  const hour = Math.floor(differ_end / 1000 / 60 / 60 % 24);
+  const day = Math.floor(differ_end / 1000 / 60 / 60 / 24);
+
+  document.querySelector('#countdown .miriseconds').textContent = String(ms).slice(-3, -1);
+  document.querySelector('#countdown .seconds').textContent = String(sec).padStart(2, '0');
+  document.querySelector('#countdown .minutes').textContent = String(min).padStart(2, '0');
+  document.querySelector('#countdown .hours').textContent = String(hour).padStart(2, '0');
+  for (i = 0; i <= document.querySelectorAll('#countdown .day').length; i++) {
+    if (document.querySelectorAll('#countdown .day')[i] != undefined)
+      document.querySelectorAll('#countdown .day')[i].textContent = String(day).padStart(2, '0');
+  }
+}

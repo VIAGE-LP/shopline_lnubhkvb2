81cf541e829d708d87ce99a1c4a22c6578e47e55
VIAGE-LP <57522097+VIAGE-LP@users.noreply.github.com>
2024-09-03 16:18:14 +0800

    Update countdown.js

--- lnubhkvb2-0904/js/countdown.js
+++ lnubhkvb2-0904/js/countdown.js
@@ -1,5 +1,5 @@
 const day_start = new Date('2024/09/03 16:10:00'); // 天倒數 開始時間
-const sec_start = new Date('2024/09/03 16:15:00'); // 秒倒數 開始時間
+const sec_start = new Date('2024/09/03 16:25:00'); // 秒倒數 開始時間
 const end = new Date('2024/09/04 23:59:59'); // 結束時間
 let now = new Date();
 
